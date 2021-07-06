561263af61ffc4c659b7afc5afe6e030b65af909
sai ram kaushik <sairamkaushik1@gmail.com>
2021-07-06 21:22:42 +0530

    modified index.hmtl and index.php

--- index.php
+++ index.php
@@ -14,17 +14,19 @@
     <form name="text-analyzer" method="POST" action="./index.php" onsubmit="return validate()">
         <div class="container">
             <h1>TEXT ANALYZER</h1>
-            <textarea name="box" class="line" placeholder="Enter the text" id="line"></textarea>
+            <textarea name="data" class="line" placeholder="Enter the text" id="box"></textarea>
             <input type="submit" value="submit" class="btn">
             <p>The length of the string is:<br>
                 <?php
-                $line = $_POST['box'];
+                error_reporting(0);
+                $data = $_POST['data'];
                 echo strlen ($line);
             ?>
             </p>
             <p1>The number of the words are:<br>
                 <?php
-                    $word = $_POST['box'];
+                    error_reporting(0);
+                    $data = $_POST['data'];
                     echo str_word_count ($word);
                 ?>
             </p1>
